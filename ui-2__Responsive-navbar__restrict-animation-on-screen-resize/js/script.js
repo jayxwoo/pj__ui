f8319ea8758cd9b1d2729d2987ff780f398cf243
jayxwoo <35472791+jayxwoo@users.noreply.github.com>
2020-03-13 07:18:36 +0900

    shortened animation stopper JS code
    - clearTimeout is not required
    - if statement is not required
    - removed unnecessary code

--- ui-2__Responsive-navbar__restrict-animation-on-screen-resize/js/script.js
+++ ui-2__Responsive-navbar__restrict-animation-on-screen-resize/js/script.js
@@ -2,25 +2,13 @@
 const menuBtn = document.querySelector('.menu-btn');
 const headerNav = document.querySelector('.header__nav')
 
-// stop transition on screen resize
-const addRemoveTransition = function () {
-    let timer = null;
-
-    const removeTransition = function () {
-        headerNav.classList.remove('stop-transition');
-        timer = null;
-    }
-
-    timer = setTimeout(removeTransition, 1000);
-
-    if (timer = null) {
-        clearTimeout(timer);
-    } else {
-        headerNav.classList.add('stop-transition');
-    }
-}
-
-window.addEventListener('resize', addRemoveTransition);
+// Stop transition and animation during screen resizing
+window.addEventListener('resize', function () {
+    document.body.classList.add('resize-animation-stopper');
+    let timer = setTimeout(function () {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 300);
+});
 
 // menu button animation
 menuBtn.addEventListener('click', function () {
